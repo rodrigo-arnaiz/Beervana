b4983c3de26039a8f7e7e574789a29c35434a47b
Matiaseze <matyaseze777@gmail.com>
2025-05-17 20:11:30 -0300

    Cambio en la config de la base de datos para conectarse con supabase

--- config/database.php
+++ config/database.php
@@ -82,7 +82,7 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
+/*         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -95,7 +95,20 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
-        ],
+        ], */
+
+        'pgsql' => [
+    'driver' => 'pgsql',
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '5432'),
+    'database' => env('DB_DATABASE', 'forge'),
+    'username' => env('DB_USERNAME', 'forge'),
+    'password' => env('DB_PASSWORD', ''),
+    'charset' => env('DB_CHARSET', 'utf8'),
+    'prefix' => '',
+    'schema' => 'public',
+    'sslmode' => 'require', // Supabase requires this :)
+],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
