b25b4ab54166dac14c1766d0fa29d650ef731844
rodrigo-arnaiz <rodrigo.arnaiz2209@gmail.com>
2025-06-05 23:22:36 -0300

    correcciones

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,13 +15,21 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+    /* 'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',request()->getHost(),
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        Sanctum::currentRequestHost(),
         Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
-    ))),
-
+    ))), */
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', implode(',', [
+        'localhost',
+        'localhost:3000',
+        '127.0.0.1',
+        '127.0.0.1:8000',
+        '::1',
+        Sanctum::currentRequestHost(),
+        Sanctum::currentApplicationUrlWithPort(),
+    ]))),
     /*
     |--------------------------------------------------------------------------
     | Sanctum Guards
